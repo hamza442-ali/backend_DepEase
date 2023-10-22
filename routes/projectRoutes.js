94936adf91eba1de66cf5aa70b5937ba4baa6069
abdullah117765 <i200457@nu.edu.pk>
2023-10-22 15:13:04 +0500

    project feature

---
+++ routes/projectRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const projectController = require('./projectController');
+
+// Create a new project
+router.post('/', projectController.createProject);
+
+// Get all projects
+router.get('/', projectController.getAllProjects);
+
+// Get a specific project by ID
+router.get('/:projectId', projectController.getProjectById);
+
+// Update a project by ID
+router.put('/:projectId', projectController.updateProjectById);
+
+// Delete a project by ID
+router.delete('/:projectId', projectController.deleteProjectById);
+
+module.exports = router;
